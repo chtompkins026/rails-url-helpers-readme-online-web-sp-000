744938792b077eabe90054907c5272719f2d8432
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-10 18:38:31 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
   
-  get '/register', to: 'users#new', as: 'register
+  get '/register', to: 'users#new', as: 'register'
   
 end
