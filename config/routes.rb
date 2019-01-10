afca0b9b6639024661f8001d4cb052468cf546b2
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-10 18:37:26 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
   
-  get '/students/:id/activate' => 'students#activate', as: 'activate_student'
+  get '/register', to: 'users#new', as: 'register
   
 end
